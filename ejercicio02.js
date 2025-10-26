bf5e2e41caf04186b7d0a7de92fc70e45420195f
walo13-Omega <braulio2407055@hybridge.education>
2025-10-26 12:49:35 -0700

    segundo cambio

--- ejercicio02.js
+++ ejercicio02.js
@@ -1,12 +1,11 @@
 // Ejercicio 2: Uso de Constantes en Funciones
 // Declara una función llamada "obtenerPI" que retorne el valor 3.1416
 
-//define tu función aquí
+function obtenerPI() {
+    const PI = 3.1416;
+    return PI;
+}
 
-
-// No modificar
-// Exporta la función para que pueda ser utilizada en otros archivos
-// Como ves, tu función debe tener el nombre obtenerPI
 module.exports = {
     obtenerPI
 };

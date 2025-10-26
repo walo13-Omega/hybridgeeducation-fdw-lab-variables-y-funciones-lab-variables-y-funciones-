f9371c6d33837947224a7de541458c3942893498
walo13-Omega <braulio2407055@hybridge.education>
2025-10-26 13:06:16 -0700

    noveno  cambio

--- ejercicio09.js
+++ ejercicio09.js
@@ -2,7 +2,9 @@
 // Declara una función llamada "calcularPromedio" que acepte 5 parámetros "n1", "n2", "n3", "n4", "n5" y devuelva el promedio de los 5 números.
 
 //escribe tu función aquí. Recuerda que el nombre de la función debe ser exactamente "calcularPromedio" y que debe recibir exactamente 5 parámetros
-
+function calcularPromedio(n1, n2, n3, n4, n5) {
+    return (n1 + n2 + n3 + n4 + n5) / 5;
+}
 
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {

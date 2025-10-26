c9471d7c4b1c9378c31e99ce0e6dc94ddd407573
walo13-Omega <braulio2407055@hybridge.education>
2025-10-26 12:51:20 -0700

    tercer cambio

--- ejercicio03.js
+++ ejercicio03.js
@@ -2,7 +2,9 @@
 // Declara una función llamada "sumar" que acepte dos parámetros (a y b) y retorne su suma
 
 //Escribe tu función aquí
-
+function sumar(a, b) {
+    return a + b;
+}
 
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {

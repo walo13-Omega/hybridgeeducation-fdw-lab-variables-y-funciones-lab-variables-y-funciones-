4a304a134245bfafc502d2e9edb7a3f2468bc1c2
walo13-Omega <braulio2407055@hybridge.education>
2025-10-26 12:57:56 -0700

    cuarto cambio

--- ejercicio04.js
+++ ejercicio04.js
@@ -2,6 +2,9 @@
 // Declara una función llamada "saludar" que acepte un parámetro "nombre" y retorne un mensaje de saludo con la estructura "Hola, nombre!".
 
 // Escribe tu función aquí. El nombre exacto debe ser saludar
+function saludar(nombre) {
+    return "Hola, " + nombre + "!";
+}
 
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {

bc828170fab8955b898aae46544cfb2499610f6a
walo13-Omega <braulio2407055@hybridge.education>
2025-10-26 13:02:43 -0700

    septimo cambio

--- ejercicio07.js
+++ ejercicio07.js
@@ -2,7 +2,9 @@
 // Declara una función llamada "esPar" que acepte un parámetro "numero" y retorne verdadero si el número es par, de lo contrario falso
 
 //escribe tu función aquí. Recuerda que el nombre de la función debe ser exactamente "esPar"
-
+function esPar(numero) {
+    return numero % 2 === 0;
+}
 
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
